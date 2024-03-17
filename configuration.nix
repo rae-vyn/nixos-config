73c432f7ae0c58bcc7c271af2fb1b54a3f482cca
Raevyn <raevyntunes@outlook.com>
2024-03-17 11:44:25 -0400

    74 current  2024-03-17 11:44:20  23.11.4874.068d4db60495  6.1.79                              *

--- configuration.nix
+++ configuration.nix
@@ -68,6 +68,11 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+  };
 
   # Enable sound with pipewire.
   sound.enable = true;
