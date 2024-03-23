5e4751212bec60b84a60fe38d0b6db616236df15
Raevyn <raevyntunes@outlook.com>
2024-03-23 07:56:10 -0400

    94 current  2024-03-23 05:37:38  23.11.5441.614b4613980a  6.1.82                              *

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.spicetify-nix.url = "github:the-argus/spicetify-nix";
+
+  outputs = { nixpkgs, ... } @inputs : {
+    # replace 'joes-desktop' with your hostname here.
+    nixosConfigurations.raevyn-pc = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = inputs;
+      modules = [ 
+        ./configuration.nix
+	./spicetify.nix
+      ];
+    };
+  };
+}
+
