624f4f0b635dd32aee6c43afdff498900517084e
Raevyn <raevyntunes@outlook.com>
2024-04-01 13:29:04 -0400

    111 current  2024-04-01 13:28:58  24.05.20240322.a3ed740   6.6.22                              *

--- configuration.nix
+++ configuration.nix
@@ -97,6 +97,16 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+  services.pipewire.wireplumber.configPackages = [
+	(pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+		bluez_monitor.properties = {
+			["bluez5.enable-sbc-xq"] = true,
+			["bluez5.enable-msbc"] = true,
+			["bluez5.enable-hw-volume"] = true,
+			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+		}
+	'')
+  ];
 
   programs.dconf.enable = true;
   services.xserver.enable = true;
