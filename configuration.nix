6750184dcef825abf5778683cb75d3e8705a75c9
Raevyn <raevyntunes@outlook.com>
2024-04-01 14:31:36 -0400

    114 current  2024-04-01 14:31:30  24.05.20240322.a3ed740   6.6.22                              *

--- configuration.nix
+++ configuration.nix
@@ -140,6 +140,8 @@
 
 
   programs.rog-control-center.enable = true;
+  programs.noisetorch.enable = true;
+
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
