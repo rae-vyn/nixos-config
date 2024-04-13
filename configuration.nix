94c52087e746471a87f67cd3fe6d9732f4f9a16e
raevyn <raevyntunes@outlook.com>
2024-04-13 08:44:49 -0400

    140 current  2024-04-13 08:24:30  24.05.19700101.dirty     6.6.22                              *

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./packages.nix
+      ./nitter.nix
     ];
   musnix.enable = true;
   hardware.bluetooth = {
