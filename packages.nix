eefc3c4302ff3c324d2126cda7dbe8335ccb74aa
raevyn <raevyntunes@outlook.com>
2024-04-17 17:14:33 -0400

    154 current  2024-04-17 17:14:20  24.05.19700101.dirty     6.6.22                              *

--- packages.nix
+++ packages.nix
@@ -58,6 +58,7 @@
         # ---------------------------------------------------------------
         nmap
         whois
+        wireshark-qt
         
         # Computer Stuff
         # Drivers, interfaces, and the like
