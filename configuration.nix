8f4c5017eb501bc3f00507d2c888537e7e57fa70
raevyn <raevyntunes@outlook.com>
2024-04-27 16:29:52 -0400

    193 current  2024-04-27 15:25:03  24.05.19700101.dirty  6.6.22                          *

--- configuration.nix
+++ configuration.nix
@@ -164,7 +164,8 @@
   };
 
   nixpkgs.config.permittedInsecurePackages = [
-     "electron-25.9.0"
+     "electron-25.9.0",
+     "pulsar-1.114.0"
   ];
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
