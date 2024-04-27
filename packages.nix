f8c6706a3faa61d6b0bc1e538a8e35ba5093e7a6
raevyn <raevyntunes@outlook.com>
2024-04-27 16:47:18 -0400

    194 current  2024-04-27 16:32:08  24.05.19700101.dirty  6.6.22                          *

--- packages.nix
+++ packages.nix
@@ -1,6 +1,6 @@
 { pkgs, ...}:
 let
-    unstable = import nixos-unstable { config = { allowUnfree = true; }; };
+    unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in
 {
     environment.systemPackages = with pkgs; [
