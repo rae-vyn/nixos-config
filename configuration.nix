c4285799d5c4044a6c8b4522d246445252913d36
raevyn <raevyntunes@outlook.com>
2024-05-03 18:59:09 -0400

    220 current  2024-05-03 18:59:03  24.05.19700101.dirty  6.6.22                          *

--- configuration.nix
+++ configuration.nix
@@ -198,7 +198,8 @@
     NIXOS_OZONE_WL = "1";
     RUSTC_WRAPPER = "sccache";
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-    OPENSSL_DIR = pkgs.openssl.dev;
+    OPENSSL_DIR = "${pkgs.openssl.dev}";
+    OPENSSL_LIB_DIR = "${pkgs.openssl.dev}/lib";
   };
 
   services.dbus.enable = true;
