95d0e0bcad2da5382f5c15e16a20585eec8d847c
raevyn <raevyntunes@outlook.com>
2024-05-14 16:19:54 -0400

    237 current  2024-05-14 11:40:08  24.05.19700101.dirty  6.6.22                          *

--- musnix.nix
+++ musnix.nix
@@ -1,19 +1,21 @@
 {
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
-    musnix  = { url = "github:musnix/musnix"; };
+    nixpkgs = {url = "github:NixOS/nixpkgs/nixos-unstable";};
+    musnix = {url = "github:musnix/musnix";};
   };
   outputs = inputs: rec {
     nixosConfigurations = {
       example-config = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules =
-          [ # ...
-            inputs.musnix.nixosModules.musnix
-            ./configuration.nix
-          ];
-        specialArgs = { inherit inputs; };
+        modules = [
+          # ...
+          inputs.musnix.nixosModules.musnix
+          ./configuration.nix
+        ];
+        specialArgs = {inherit inputs;};
       };
     };
   };
+}
 # ...
+
